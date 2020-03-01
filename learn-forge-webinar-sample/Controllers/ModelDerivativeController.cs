df81cdb721f3f6281504418211392088c93544b3
Eason Kang <eason.kang@autodesk.com>
2020-03-01 23:37:06 +0800

    Backend Teamplate Setting

---
+++ learn-forge-webinar-sample/Controllers/ModelDerivativeController.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Forge;
+using Autodesk.Forge.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace learn_forge_webinar_sample.Controllers
+{
+    [ApiController]
+    public class ModelDerivativeController : ControllerBase
+    {
+        /// <summary>
+        /// Start the translation job for a give bucketKey/objectName
+        /// </summary>
+        /// <param name="objModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/forge/modelderivative/jobs")]
+        public async Task<dynamic> TranslateObject([FromBody]TranslateObjectModel objModel)
+        {
+
+        }
+
+        /// <summary>
+        /// Model for TranslateObject method
+        /// </summary>
+        public class TranslateObjectModel
+        {
+            public string bucketKey { get; set; }
+            public string objectName { get; set; }
+        }
+    }
+}
---
+++ learn-forge-webinar-sample/Controllers/OAuthController.cs
@@ -0,0 +1,52 @@
+﻿using Autodesk.Forge;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace learn_forge_webinar_sample.Controllers
+{
+    [ApiController]
+    public class OAuthController : ControllerBase
+    {
+        // As both internal & public tokens are used for all visitors
+        // we don't need to request a new token on every request, so let's
+        // cache them using static variables. Note we still need to refresh
+        // them after the expires_in time (in seconds)
+        private static dynamic InternalToken { get; set; }
+        private static dynamic PublicToken { get; set; }
+
+        /// <summary>
+        /// Get access token with public (viewables:read) scope
+        /// </summary>
+        [HttpGet]
+        [Route("api/forge/oauth/token")]
+        public async Task<dynamic> GetPublicAsync()
+        {
+
+        }
+
+        /// <summary>
+        /// Get access token with internal (write) scope
+        /// </summary>
+        public static async Task<dynamic> GetInternalAsync()
+        {
+
+        }
+
+        /// <summary>
+        /// Get the access token from Autodesk
+        /// </summary>
+        private static async Task<dynamic> Get2LeggedTokenAsync(Scope[] scopes)
+        {
+
+        }
+
+        /// <summary>
+        /// Reads appsettings from web.config
+        /// </summary>
+        public static string GetAppSetting(string settingKey)
+        {
+            return Environment.GetEnvironmentVariable(settingKey);
+        }
+    }
+}
